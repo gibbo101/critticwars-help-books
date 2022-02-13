5be688544a9e5082137eb00ad08e24fbcd6db914
Luke Gibson <lukegibson101@gmail.com>
2022-02-13 18:55:54 +0000

    set up admin panel

--- help/admin.py
+++ help/admin.py
@@ -1,21 +1,29 @@
 from django.contrib import admin
-from .models import Category, Subject, Comments
+from .models import Category, Subject, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 
+    list_display = ('title', 'slug')
+    search_fields = ['title']
     prepopulated_fields = {'slug': ('title',)}
 
 
 @admin.register(Subject)
 class SubjectAdmin(SummernoteModelAdmin):
-    
+
+    list_display = ('title', 'slug', 'category', 'created_date', 'author')
+    search_fields = ['title']
+    list_filter = ('category',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
 
 
-@admin.register(Comments)
+@admin.register(Comment)
 class CommentAdmin(SummernoteModelAdmin):
 
+    list_display = ('subject', 'author', 'created_date', 'content')
+    list_filter = ('subject', 'created_date')
+    search_fields = ['content']
     summernote_fields = ('content')
