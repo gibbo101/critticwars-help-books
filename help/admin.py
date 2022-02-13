9b301b24239c3d6d870019b7acaf0ac0faa0251e
Luke Gibson <lukegibson101@gmail.com>
2022-02-13 17:14:48 +0000

    make tables visible add summer note to admin panel

--- help/admin.py
+++ help/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Subject, Comments
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Category)
+
+
+@admin.register(Subject)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+
+@admin.register(Comments)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
