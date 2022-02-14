5bb137278ba29a93cede7c5d47baa4832ec4a8be
Luke Gibson <lukegibson101@gmail.com>
2022-02-14 13:32:50 +0000

    set Category view

--- help/views.py
+++ help/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Category
 
-# Create your views here.
+
+class CategoryList(generic.ListView):
+    model = Category
+    queryset = Category.objects.order_by('id')
+    template_name='base.html'
